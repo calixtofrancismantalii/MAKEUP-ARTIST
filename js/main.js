f4c54885c64134328a9622e0f423d1ea5aa05443
Cyries <cyriesmadrid@gmail.com>
2020-07-11 21:55:29 +0800

    currently working mobile view with java script

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function showMenu(){
+//    var ul;
+//    ul = document.getElementById("ul");
+//    
+//    
+//    if(ul.style.height = "0px"){
+//        ul.style.height = "500px";
+//    }
+//    else{
+//        ul.style.height = "";
+//    }
+//    
+    
+}
