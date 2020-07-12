84c051fac95f28853a558476a311aaf0f3095d64
Cyries <cyriesmadrid@gmail.com>
2020-07-12 08:20:33 +0800

    currently working on script

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-function showMenu(){
-//    var ul;
-//    ul = document.getElementById("ul");
-//    
-//    
-//    if(ul.style.height = "0px"){
-//        ul.style.height = "500px";
-//    }
-//    else{
-//        ul.style.height = "";
-//    }
-//    
+function showMenu() {
+    var ul;
+    ul = document.getElementById("ul");
     
+    if(ul.style.height === "0px"){
+        ul.style.height = "290px";
+    }
+    else {
+        ul.style.height = "0px";
+    }
 }
+
+
