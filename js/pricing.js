66b979cbc13dadfa6a7655f18b548e25aa44d56c
Cyries <cyriesmadrid@gmail.com>
2020-08-09 20:42:34 +0800

    currently working js file for pricing

--- js/pricing.js
+++ js/pricing.js
@@ -12,3 +12,37 @@ function showMenu(){
      
     
 }
+
+
+
+function on(){
+    
+    var on, off;
+    on = document.getElementById("on");
+    off = document.getElementById("off");
+    
+    if(on.style.display == ""){
+        on.style.display = "none";
+        off.style.display = "block"
+    }
+    else{
+        off.style.display = "";
+        on.style.display = "block";
+    }
+}
+
+function off(){
+    var on, off;
+    on = document.getElementById("on");
+    off =  document.getElementById("off");
+    
+    if(off.style.display == "none"){
+        off.style.display = "block";
+    }
+    else{
+        off.style.display = "block";
+        on.style.display = "none";
+    }
+}
+
+
