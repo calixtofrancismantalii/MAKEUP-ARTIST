e3a0203a1acbc5846c060fb393b9c06b19b25956
Cyries <cyriesmadrid@gmail.com>
2020-08-31 22:40:25 +0800

    fixing map in contact pae

--- js/contact.js
+++ js/contact.js
@@ -15,9 +15,21 @@ function showMenu() {
 //function myMap(){
 //    
 //    var mapProp = {
-//        center: new google.maps.LatLang(51.047310, -114.057968),
+//        center: new google.maps.LatLng(51.047310, -114.057968),
 //        zoom: 5;
 //    };
 //    
 //    var map = new google.maps.Map(document.getElementById("mapa"), mapProp);
+//}
+
+
+
+
+//
+//function myMap(){
+//    var mapProp = {
+//        center: new google.maps.LatLng(51.047310, -114.057968),
+//        zoom: 5;
+//    };
+//    var map = new google.maps.Map(document.getElementById("mkaMap"), mapProp);
 //}
